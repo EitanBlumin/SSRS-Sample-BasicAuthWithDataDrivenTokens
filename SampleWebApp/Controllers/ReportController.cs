0b9e33103be1bec868d7e3884fca05857b468947
Eitan Blumin <eitan@madeiradata.com>
2020-08-19 14:02:51 +0300

    Added stub projects

---
+++ SampleWebApp/Controllers/ReportController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SampleWebApp.Controllers
+{
+    public class ReportController : Controller
+    {
+        // GET: Report
+        [Authorize]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
